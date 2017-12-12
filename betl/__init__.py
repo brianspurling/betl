5691f5d18ed2c7215f0e075c2dee0737a577f881
brianspurling <brian.spurling@gmail.com>
2017-12-12 17:37:18 +0000

    Expose the getETLDBConnection function to the application

--- betl/__init__.py
+++ betl/__init__.py
@@ -4,4 +4,6 @@ from .control import processArgs
 from .control import addDefaultExtractToSchedule
 from .control import run
 
+from .utilities import getEtlDBConnection
+
 from .scheduler import scheduleDataFlow
