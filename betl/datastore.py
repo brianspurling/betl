9125c3f90b5f192da20a2fb25f84e986997cca52
brianspurling <brian.spurling@gmail.com>
2018-04-18 13:16:26 +0100

    Bug fix on earlier change: cannot get sqlite to open readonly, so removing

--- betl/datastore.py
+++ betl/datastore.py
@@ -89,11 +89,19 @@ class SqliteDatastore(Datastore):
         self.dbID = dbID
         self.path = path
         self.filename = filename
-        readOnlyString = ''
-        if isSrcSys:
-            readOnlyString = '?mode=ro'
-        self.conn = sqlite3.connect(path + '/' + filename + readOnlyString,
-                                    uri=True)
+        # TODO: you're supposed to be able to connect in read-only mode using:
+        # readOnlyString = ''
+        # if isSrcSys:
+        #    readOnlyString = '?mode=ro'
+        # self.conn = sqlite3.connect('file:/' + path + filename +
+        #                             readOnlyString,
+        #                             uri=True)
+        # But it doesn't seem to work - just tries to open a database of that
+        # full name (including query string). See:
+        # https://stackoverflow.com/questions/10205744/
+        #   opening-sqlite3-database-from-python-in-read-only-mode
+
+        self.conn = sqlite3.connect(path + filename)
 
     def commit(self):
         self.conn.commit()
