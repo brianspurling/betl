c3682fe4878c9f8f9c016e18e07e631b457dea4c
brianspurling <brian.spurling@gmail.com>
2018-06-13 11:59:48 +0100

    bug fix: row limit from gsheets data sources

--- betl/gsheetIO.py
+++ betl/gsheetIO.py
@@ -5,8 +5,8 @@ def readDataFromWorksheet(worksheet, limitdata=None):
 
     data = worksheet.get_all_values()
     if limitdata is not None:
-        rowLimit = len(data)
-    else:
         rowLimit = limitdata
+    else:
+        rowLimit = len(data)
 
     return pd.DataFrame(data[1:rowLimit], columns=data[0])
